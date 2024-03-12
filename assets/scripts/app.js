51fd83eaed74f80a357d1fd22401285ac98c55de
Wowxqqqq <tristan@osidon.com>
2024-03-12 17:47:19 +0200

    Moving forward with functions.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,17 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function add (num1, num2) {
+    const result = num1 + num2;
+    return result;
+}
+
+const additionResult = add(1, 2);
+
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
 let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
 let errorMessage = 'An error \n' + 'occured!';
 
 outputResult(currentResult, calculationDescription);
+
