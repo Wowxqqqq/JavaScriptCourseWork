ff9b34e1dbb3944e9ac1445dffcf92520bd6f052
Wowxqqqq <tristan@osidon.com>
2024-03-13 16:33:52 +0200

    Busy with functions and how variables interact with them.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,17 +1,9 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add (num1, num2) {
-    const result = num1 + num2;
-    return result;
+function add () {
+    const result = currentResult + userInput.value;
+    outputResult(currentResult, '');
 }
 
-const additionResult = add(1, 2);
-
-currentResult = (currentResult + 10) * 3 / 2 - 1;
-
-let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
-let errorMessage = 'An error \n' + 'occured!';
-
-outputResult(currentResult, calculationDescription);
-
+addBtn.addEventListener('click', add);
